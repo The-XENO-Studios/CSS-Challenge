9c07efb17c8ae80878749f48fccf1a122f316e6d
The-XENO-Studios <thexenostudios@gmail.com>
2024-01-06 11:53:15 +0600

    Animation Updated

--- index.js
+++ index.js
@@ -21,4 +21,44 @@ timeline
       duration: 0.3,
     }
   )
-  .fromTo(".Last", { opacity: "0" }, { opacity: "1" });
+  .fromTo(".Last", { opacity: "0" }, { opacity: "1", onComplete: animate });
+
+function animate() {
+  gsap.to(".char", {
+    y: "-100%",
+    stagger: 0.05,
+    delay: 0.6,
+    duration: 0.4,
+    onComplete: reverse,
+  });
+}
+
+function reverse() {
+  gsap.to(".char", {
+    y: "0%",
+    stagger: 0.05,
+    delay: 0.6,
+    duration: 0.4,
+    onComplete: downAnimate,
+  });
+}
+
+function downAnimate() {
+  gsap.to(".char", {
+    y: "100%",
+    stagger: 0.05,
+    delay: 0.6,
+    duration: 0.4,
+    onComplete: upreverse,
+  });
+}
+
+function upreverse() {
+  gsap.to(".char", {
+    y: "0%",
+    stagger: 0.05,
+    delay: 0.6,
+    duration: 0.4,
+    onComplete: animate,
+  });
+}
